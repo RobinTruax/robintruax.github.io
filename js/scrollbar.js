066c4f1c1ebcb4bdf62514ef4642587c7068c6ac
RobinTruax <robinaldabanx@gmail.com>
2024-05-21 16:20:21 -0700

    Simplified-Test

--- js/scrollbar.js
+++
@@ -1,8 +0,0 @@
-// Constants
-book = document.querySelector(".book");
-scrollbar = document.querySelector("#scrollbar");
-
-// Scrollbar
-book.addEventListener("scroll", () => {
-    scrollbar.style.top = (100*book.scrollTop/book.scrollHeight).toString() + "%";
-});
