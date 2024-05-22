e681ce3085b87d555a89707fc8ba26e463b13797
RobinTruax <robinaldabanx@gmail.com>
2024-05-22 00:12:15 -0700

    Test.

--- resize.js
+++ resize.js
@@ -10,3 +10,12 @@ window.addEventListener('touchmove', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+// Constants
+book = document.querySelector(".book");
+scrollbar = document.querySelector("#scrollbar");
+
+// Scrollbar
+book.addEventListener("scroll", () => {
+    scrollbar.style.top = (100*book.scrollTop/book.scrollHeight).toString() + "%";
+});
