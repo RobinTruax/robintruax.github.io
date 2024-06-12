d2a7b1fe07a6a05af661ed5cd2286396e2624a61
RobinTruax <robinaldabanx@gmail.com>
2024-06-12 12:46:57 -0700

    Dividers prototype.

--- js/resize.js
+++ js/resize.js
@@ -1,11 +1,14 @@
+// Custom Variable for Height
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+// Update on Resize
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
+// Update on Movement
 window.addEventListener('touchmove', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -17,9 +20,6 @@ scrollbar = document.querySelector("#scrollbar");
 
 // Scrollbar
 book.addEventListener("scroll", () => {
-    // setTimeout(function(){
-    //     alert(scrollbar.style.height);
-    // }, 5000);
     scrollbar.style.top = (window.innerHeight*book.scrollTop/book.scrollHeight).toString() + "px";
     scrollbar.style.height = (window.innerHeight*window.innerHeight/book.scrollHeight).toString() + "px";
 });
