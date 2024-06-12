d2795cc42fb4d3334b5fb21a0f314c9b5ddf8d42
RobinTruax <robinaldabanx@gmail.com>
2024-06-12 16:51:34 -0700

    Testing...

--- js/dividers.js
+++ js/dividers.js
@@ -1,5 +1,6 @@
 // Contrast
 book = document.querySelector(".book");
+pages = document.getElementsByClassName("page")
 d1 = document.querySelector("#d1");
 d2 = document.querySelector("#d2");
 
@@ -40,40 +41,49 @@ book.addEventListener("scroll", () => {
     d2.style.clipPath = polyprogress(0.3*window.innerHeight, 115, -105);
 });
 
-// // Snap Target
-// function snaptarget(start, end){
-//     wh = window.innerHeight;
-//     if(end > start){
-//         if(end > start + wh*0.2){
-//             return Math.ceil(end/wh)*wh;
-//         } else {
-//             return Math.floor(end/wh)*wh;
-//         }
-//     } else {
-//         if(end < start - wh*0.2){
-//             return Math.floor(end/wh)*wh;
-//         } else {
-//             return Math.ceil(end/wh)*wh;
-//         }
-//     }
-// }
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-// // Scroll Snap
-// var timer = null;
-// var scrollstart = -1;
-// book.addEventListener('scroll', function() {
-//     if(scrollstart == -1){
-//         scrollstart = book.scrollTop;
-//     }
-//     if(timer !== null) {
-//         clearTimeout(timer);        
-//     }
-//     timer = setTimeout(function() {
-//         book.scrollTo({
-//             top: snaptarget(scrollstart, book.scrollTop),
-//             left: 100,
-//             behavior: "smooth",
-//         });
-//         scrollstart = -1;
-//     }, 100);
-// }, false);
+if(isMobile){
+    alert("mobile");
+    book.scrollSnapType = "y mandatory"
+} else {
+    alert("not mobile");
+    // Snap Target
+    function snaptarget(start, end){
+        wh = window.innerHeight;
+        if(end > start){
+            if(end > start + wh*0.2){
+                return Math.ceil(end/wh)*wh;
+            } else {
+                return Math.floor(end/wh)*wh;
+            }
+        } else {
+            if(end < start - wh*0.2){
+                return Math.floor(end/wh)*wh;
+            } else {
+                return Math.ceil(end/wh)*wh;
+            }
+        }
+    }
+
+    // Scroll Snap
+    var timer = null;
+    var scrollstart = -1;
+    book.addEventListener('scroll', function() {
+        if(scrollstart == -1){
+            scrollstart = book.scrollTop;
+        }
+        if(timer !== null) {
+            clearTimeout(timer);        
+        }
+        timer = setTimeout(function() {
+            book.scrollTo({
+                top: snaptarget(scrollstart, book.scrollTop),
+                left: 100,
+                behavior: "smooth",
+            });
+            scrollstart = -1;
+        }, 100);
+    }, false);
+
+}
