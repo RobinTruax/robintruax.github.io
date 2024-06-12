06c0ae48348fc3e7cb92791cf88a9645d8989d37
RobinTruax <robinaldabanx@gmail.com>
2024-06-12 12:58:28 -0700

    Flicker fix.

--- js/dividers.js
+++ js/dividers.js
@@ -21,6 +21,25 @@ function bezier(t, initial, p1, p2, final) {
 
 // Animation
 book.addEventListener("scroll", () => {
-    d1.style.top = bezier(progress(0), 110, 0, 0, -85).toString() + "%";
-    d2.style.top = bezier(progress(0.3*window.innerHeight), 115, 0, 0, -85).toString() + "%";
+    // Progress
+    progress1 = bezier(progress(0), 110, 0, 0, -85);
+    progress2 = bezier(progress(0.3*window.innerHeight), 115, 0, 0, -85);
+
+    // Core
+    d1.style.top = progress1.toString() + "%";
+    d2.style.top = progress2.toString() + "%";
+
+    // Flicker Protection (Divider 1)
+    if(progress1 < -80){
+        d1.style.opacity = "0";
+    } else if(progress1 > 105) {
+        d1.style.opacity = "1";
+    }
+
+    // Flicker Protection (Divider 2)
+    if(progress2 < -80){
+        d2.style.opacity = "0";
+    } else if(progress2 > 105) {
+        d2.style.opacity = "1";
+    }
 });
